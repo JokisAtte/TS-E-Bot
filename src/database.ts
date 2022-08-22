17e6243ea2171171ebb1d9356fc51fff967d318b
Atte Jokinen <atte.jokinen@tuni.fi>
2022-08-22 22:06:52 +0300

    pohja kaikille db funktioille

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,40 @@
+import { model, Schema } from "mongoose";
 
-
-const database = () => {
-    
+interface IUser {
+  handle: string;
+  tg_id: string;
+  balance: number;
 }
 
-export default database
+interface IPayment {
+  date: string;
+  amount: number;
+}
+
+const userSchema = new Schema<IUser>({
+  handle: { type: String, required: true },
+  tg_id: { type: String, required: true },
+  balance: { type: Number, required: true },
+});
+
+const User = model<IUser>("User", userSchema);
+
+export const newUser = () => {
+    
+};
+
+export const findUser = () => {
+    
+};
+
+export const newPurchase = () => {
+
+}
+
+export const getUsers = () => {
+
+}
+
+export const payDebts = () => {
+
+}
